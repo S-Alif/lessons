33b61ac0a33e7e52be894f85ceb6589808ac8aa3
S-Alif <alif160038@gmail.com>
2024-12-24 00:23:56 +0600

    dynamic html insertion easy method

--- 23-12-24/script.js
+++ 23-12-24/script.js
@@ -5,35 +5,53 @@ const userDiv = document.querySelector('.show-users')
 const users = [
     {name: 'John', age: 30},
     {name: 'Jane', age: 25},
-    {name: 'Mike', age: 35}
+    {name: 'Mike', age: 35},
+    {name: 'Tinny', age: 85},
 ]
 
+// easy one
 function displayUsers() {
-
     userDiv.classList.add("display-cards")
 
     users.map(function(value, index) {
+        let htmlMarkup = `
+            <div class="user-card" id="${index}">
+                <h3>${value.name}</h3>
+                <p>Age: ${value.age}</p>
+            </div>
+        `
 
-        // create h3 tag for name
-        let h3 = document.createElement('h3')
-        h3.textContent = value.name
-
-        // create p tag for age
-        let p_tag = document.createElement('p')
-        p_tag.textContent = "Age : " + value.age
-        p_tag.id = index
-
-        // create a div to wrap h3 and p tag
-        let wrapperDiv = document.createElement('div')
-        wrapperDiv.classList.add('user-card')
-
-        wrapperDiv.append(h3)
-        wrapperDiv.append(p_tag)
-
-        // finally insert the wrapper into the user div
-        userDiv.append(wrapperDiv)
+        userDiv.insertAdjacentHTML("beforeend", htmlMarkup)
     })
+
 }
+
+// function displayUsers() {
+
+//     userDiv.classList.add("display-cards")
+
+//     users.map(function(value, index) {
+
+//         // create h3 tag for name
+//         let h3 = document.createElement('h3')
+//         h3.textContent = value.name
+
+//         // create p tag for age
+//         let p_tag = document.createElement('p')
+//         p_tag.textContent = "Age : " + value.age
+//         p_tag.id = index
+
+//         // create a div to wrap h3 and p tag
+//         let wrapperDiv = document.createElement('div')
+//         wrapperDiv.classList.add('user-card')
+
+//         wrapperDiv.append(h3)
+//         wrapperDiv.append(p_tag)
+
+//         // finally insert the wrapper into the user div
+//         userDiv.append(wrapperDiv)
+//     })
+// }
 
 
 
