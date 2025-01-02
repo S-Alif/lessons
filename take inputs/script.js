251d2d096aa527ce1c8148db51df4ebf5f563373
S-Alif <alif160038@gmail.com>
2025-01-02 23:06:07 +0600

    text input and display

--- take inputs/script.js
+++ take inputs/script.js
@@ -2,8 +2,16 @@
 const fname = document.getElementById("fname")
 const lname = document.getElementById("lname")
 const submitBtn = document.getElementById("submit-btn")
+const displayDiv = document.getElementById("display-data")
 
-console.log({fname})
+function displayUserData(data){
+    console.log(data)
+    displayDiv.innerHTML = `
+        <h2>User Details:</h2>
+        <p>First Name: ${data.firstName}</p>
+        <p>Last Name: ${data.lastName}</p>
+    `
+}
 
 // submit button click / form submit
 submitBtn.onclick = function (){
@@ -12,5 +20,8 @@ submitBtn.onclick = function (){
 
     if (firstName.trim().length <= 2 || lastName.trim().length <= 2) return alert("Please enter something")
 
-    alert(`${firstName} ${lastName}`)
+    displayUserData({
+        firstName,
+        lastName
+    })
 }
