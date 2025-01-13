a794fd6d58e3f15aec0d1a97ef61b0a7840c9d76
S-Alif <alif160038@gmail.com>
2025-01-14 00:00:11 +0600

    todo remove and add from list

--- todo-list-app/script.js
+++ todo-list-app/script.js
@@ -1,14 +1,22 @@
 const todoForm = document.getElementById("todo-form")
 
-const generateListItem = (listItem) => {
+let todoListArray = []
+
+const generateListItem = () => {
     const displayDiv = document.getElementById("display-list")
-    let markUp = `
-        <div class="list-item">
-            <p>${listItem.todo}</p>
-            <p>${listItem.date}</p>
-        <div>
-    `
-    displayDiv.insertAdjacentHTML("beforeend", markUp) 
+
+    displayDiv.innerHTML = ""
+
+    todoListArray.map((todo, index) => {
+        let markUp = `
+            <div class="list-item" id="${index}">
+                <p>${todo.todo}</p>
+                <p>${todo.date}</p>
+                <button onclick="removeTodoFromList('${todo.todo}')">Remove</button>
+            <div>
+        `
+        displayDiv.insertAdjacentHTML("beforeend", markUp) 
+    })
 }
 
 todoForm.onsubmit = (event) => {
@@ -19,8 +27,24 @@ todoForm.onsubmit = (event) => {
 
     // make an object of the data inputs from the form
     let data = Object.fromEntries(formData)
-    console.log(data)
+
+    // check data
+    if(data.todo.trim() == "" || data.todo.trim().length <= 5){
+        return alert("Please enter a valid todo item with a minimum length of 6 characters.")
+    }
+    if(data.date.trim() == ""){
+        return alert("Date cannot be empty")
+    }
+
+    todoListArray.push(data)
+    console.log(todoListArray)
 
     todoForm.reset()
-    generateListItem(data)
+    generateListItem()
+}
+
+// remove a todo list
+const removeTodoFromList = (todo) => {
+    todoListArray = todoListArray.filter(item => item.todo !== todo)
+    generateListItem()
 }
